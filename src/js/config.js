891a48a18e186af884d2b0d8ee490b2c99e4b36a
Martha <martasobotyk@gmail.com>
2023-10-08 21:02:30 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '431b1b42-4c70-483f-a759-f044faa8e88a';
+export const MODAL_CLOSE_SEC = 2.5;
